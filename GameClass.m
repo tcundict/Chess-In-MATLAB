7023246aa9509dd39ec15b179850e3167fb9e43f
ASackofPotatoes <tcundict@gmail.com>
2021-11-23 23:53:08 -0700

    Added relevant properties and method to update boards

--- GameClass.m
+++ GameClass.m
@@ -1,14 +1,28 @@
-classdef GameClass < handle
+classdef GameClass
     properties
-    Board = zeros(8,8);
-    Turn                            %0 = white, 1 = black
-    Castling
-    EnPassant
-    HalfMove
-    FullMove
-%     pBit = (obj.Board ~= 0);
-%     wpBit = obj.pBit == (obj.Board < 16);
-%     bpBit = obj.pBit == (obj.Board > 16);
+        Board = zeros(8,8);
+        Turn                            %0 = white, 1 = black
+        Castling                        %1x4 cell array
+        EnPassant                       %%%TODO%%%
+        HalfMove                        %one double
+        FullMove                        %one double
+        Victory  = 0                    %0 = continuing, 1 = game is over
+        wpList                          %white piece list
+        bpList                          %black piece list
+        pBit                            %bitboard of pieces
+        wpBit                           %bitboard of white pieces
+        bpBit                           %bitboard of black pieces
+        moveList
+        
+        
+    end
+    methods
+        function game = updateState(game,newBoard)
+            game.Board = newBoard;
+            game.pBit = game.Board ~= 0;
+            game.wpBit = (game.pBit == (game.Board < 16));
+            game.bpBit = game.Board > 16;   
+            game.moveList = moveGeneration(game);
+        end
     end
 end
-        
