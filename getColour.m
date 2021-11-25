e11ab1a56551041c76156220c97f87ce04e11d85
ASackofPotatoes <tcundict@gmail.com>
2021-11-24 20:23:41 -0700

    Added header and clarified comments

--- getColour.m
+++ getColour.m
@@ -1,5 +1,7 @@
-
+%takes a numerical representation of a piece and gets the colour of the
+%piece by looking at its 5th bit, which represents colour
+%0 = white, 1 = black
 function bitFive = getColour(num)
 num = uint8(num);
 bitFive = bitand(num, 0b10000);  %check what bit is in 5th position
-bitFive = bitshift(bitFive, -4); %pull bitFive to the first position
+bitFive = bitshift(bitFive, -4); %extract the 5th bit
