fe634a2d66dbf8b6c04a8f027903c2713e7a7fad
ASackofPotatoes <tcundict@gmail.com>
2021-11-26 21:59:29 -0700

    Restructured methods, added methods to legalize and make moves

--- GameClass.m
+++ GameClass.m
@@ -13,16 +13,30 @@ classdef GameClass
         wpBit                           %bitboard of white pieces
         bpBit                           %bitboard of black pieces
         moveList
-        
-        
     end
     methods
-        function game = updateState(game,newBoard)
+        %           Currently missing:
+        %               Checkmate
+        %               En Passant
+        %               Draws
+        %               Castling ability
+        %               probably more, can't think of any rn          
+        function game = updateBoards(game,newBoard)
             game.Board = newBoard;
             game.pBit = game.Board ~= 0;
             game.wpBit = (game.pBit == (game.Board < 16));
             game.bpBit = game.Board > 16;   
+        end 
+        function game = makeMove(game,move)
+            piece = game.Board(move(1),move(2));
+            game.Board(move(1),move(2)) = 0;
+            game.Board(move(3),move(4)) = piece;
+            game = updateBoards(game,game.Board);
+            game.Turn = ~game.Turn; %Change turns
+        end
+        function game = createLegalMoves(game)
             game.moveList = moveGeneration(game);
+            game.moveList = filterForChecks(game);
         end
     end
 end
